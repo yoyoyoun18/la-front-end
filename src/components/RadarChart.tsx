0a9689aa6b5d6228eff72affa615236dfa7853e2
kimyoungjo <yoyounn88@gmail.com>
2024-09-03 14:09:36 +0900

    feat: n각형 차트 컴포넌트 구현

---
+++ src/components/RadarChart.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Radar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+const RadarChart = () => {
+  const data = {
+    labels: ["우호성", "신경성", "개방성", "외향성", "성실성"],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [65, 59, 90, 81, 56],
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      r: {
+        angleLines: { display: true },
+        suggestedMin: 0,
+        suggestedMax: 100,
+      },
+    },
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto bg-white p-4">
+      <h2 className="text-xl font-bold mb-4 text-start text-black">
+        당신의 성향
+      </h2>
+      <Radar data={data} options={options} />
+    </div>
+  );
+};
+
+export default RadarChart;
