43054f235dcc9196cc9b191d2274c1589327188e
kimyoungjo <yoyounn88@gmail.com>
2024-09-03 14:31:37 +0900

    feat: /statistics/alluser page 구현

--- src/pages/AllUserStatistics.jsx
+++ src/pages/AllUserStatistics.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CharacterRankingTable from "../components/CharacterRankingTable";
 
 function AllUserStatistics() {
-  return <div>AllUserStatistics</div>;
+  return (
+    <div className="h-screen w-screen bg-black flex justify-center items-center flex-col">
+      <CharacterRankingTable />
+    </div>
+  );
 }
 
 export default AllUserStatistics;
