c8e52ad2b7d74b1bafe72b66f1bef0957953ff9d
kimyoungjo <yoyounn88@gmail.com>
2024-09-05 15:23:33 +0900

    style: tailwind config에 커스텀 애니메이션 기능 추가(챗봇 관련)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "scale-up-br": {
+          "0%": { transform: "scale(0.5)", transformOrigin: "100% 100%" },
+          "100%": { transform: "scale(1)", transformOrigin: "100% 100%" },
+        },
+      },
+      animation: {
+        "scale-up-br":
+          "scale-up-br 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",
+      },
+    },
   },
   plugins: [],
 };
